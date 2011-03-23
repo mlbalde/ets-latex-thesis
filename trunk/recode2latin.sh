6be64ade6c08215dd32cc00326c00c02db2f4cd7
remi.menegon@gmail.com <remi.menegon@gmail.com@3fd9b9fa-d24b-b53a-6e30-6269222da114>
2011-03-23 01:26:49 +0000

    Patch de Tennesse intégré (problème d'échappement dans le script recode)

--- trunk/recode2latin.sh
+++ trunk/recode2latin.sh
@@ -3,7 +3,7 @@
 # Class
 cp -f thETS-utf8.cls thETS-latin1.cls
 sed -i 's/thETS-utf8/thETS-latin1/g' thETS-latin1.cls
-sed -i 's/RequirePackage[utf8]/RequirePackage[latin1]/g' thETS-latin1.cls
+sed -i 's/RequirePackage\[utf8\]/RequirePackage\[latin1\]/g' thETS-latin1.cls
 recode utf8..latin1 thETS-latin1.cls
 
 # Tex
